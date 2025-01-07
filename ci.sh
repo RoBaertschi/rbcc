c35db82a5d8f1fe2e577ed22d4ae50e9c5d79dbc
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-01-07 13:58:07 +0100

    made ci downloads cachable

--- ci.sh
+++ ci.sh
@@ -1,5 +1,13 @@
-curl -OL https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip
-unzip ninja-linux.zip
-export PATH="$(pwd):$PATH"
 
-./build.lua
+if [[ $1 = "setup" ]]; then
+    if test -a ./ninja; then
+        exit 0
+    fi
+
+    curl -OL https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip
+    unzip ninja-linux.zip
+    rm ninja-linux.zip
+elif [[ $1 = "build" ]]; then
+    export PATH="$(pwd):$PATH"
+    ./build.lua
+fi
