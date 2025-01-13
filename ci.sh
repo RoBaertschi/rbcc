9ad8b8506a5a3376873292af19a2a8d94dae98fa
Robin <rtmbaertschi007@gmail.com>
2025-01-13 20:52:29 +0100

    cache fasm

--- ci.sh
+++ ci.sh
@@ -1,12 +1,18 @@
 
 if [[ $1 = "setup" ]]; then
-    if test -a ./ninja; then
+    if test -a ./ninja && test -a ./fasm; then
         exit 0
     fi
 
     curl -OL https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip
     unzip ninja-linux.zip
     rm ninja-linux.zip
+
+    curl -OL https://flatassembler.net/fasm-1.73.32.tgz
+    tar -xf fasm-1.73.32.tgz -o fasm
+    mv fasm fasm-1.73.32
+    ln -s fasm-1.73.32/fasm fasm
+    rm fasm-1.73.32.tgz
 elif [[ $1 = "build" ]]; then
     export PATH="$(pwd):$PATH"
     ./build.lua
